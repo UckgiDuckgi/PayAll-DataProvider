6128e301eb41a1b8058c5282e488b1019b2b3b95
insun-k <kis8294@naver.com>
2025-01-09 10:24:56 +0900

    first commit

---
+++ src/main/java/com/example/PayAll_DataProvider/PayAllDataProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.PayAll_DataProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAllDataProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayAllDataProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PayAll_DataProvider/PayAllDataProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PayAll_DataProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayAllDataProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
